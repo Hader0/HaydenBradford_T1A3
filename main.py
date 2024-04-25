8cda2ad2f5faf70d4bd0faf6eff77d7d9257afd1
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-04-25 10:59:30 +1000

    Created menu function with 6 inputs accepted. Created a CSV to outline what the entries will be

---
+++ main.py
@@ -0,0 +1,13 @@
+print("Welcome to your Pokedex!\n")
+
+def create_menu():
+    print("1. Enter 1 to view your Pokedex")
+    print("2. Enter 2 to add a Pokemon to your Pokedex")
+    print("3. Enter 3 to remove a Pokemon from your Pokedex")
+    print("4. Enter 4 to see the strengths of a Pokemon")
+    print("5. Enter 5 to see the weaknesses of a Pokemon")
+    print("6. Enter 6 to exit\n")
+
+    user_choice = input("Enter your selection: ")
+    print(" ")
+    return user_choice
